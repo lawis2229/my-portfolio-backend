67842c5970c04d01c913a4f108eb602c6390c310
Mark Laurence Lawis <lawis32918@gmail.com>
2025-12-22 22:53:23 +0800

    email update

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,31 @@
 
 use App\Http\Controllers\EmailsController;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Mime\Part\TextPart;
+use Illuminate\Support\Facades\Mail;
+
 
 Route::get('/example', function () {
     echo 'My API is Working!';
 });
 
 Route::post('/sent-email', [EmailsController::class, 'sendMessage']);
+
+Route::post('/example-sent-email', function () {
+    $data = [
+        'name' => 'Ryanle Lawis',
+        'email' => 'ryan@gmail.com',
+        'message' => 'Hello! Message Received!'
+    ];
+
+    Mail::send([], [], function ($message) use ($data) {
+        $message->to('lawis32918@gmail.com')
+            ->subject('About your Portfolio')
+            ->setBody(new TextPart(
+                "Name: {$data['name']}\nEmail: {$data['email']}\n\n{$data['message']}",
+                'plain'
+            ));
+    });
+
+    return response()->json(['message' => 'Email sent successfully']);
+});
